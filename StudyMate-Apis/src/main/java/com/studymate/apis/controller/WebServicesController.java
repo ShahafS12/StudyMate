c459545e601bacbe19178e88c81d78395253c26a
AmitLevv <100372028+AmitLevv@users.noreply.github.com>
2024-05-10 16:14:42 +0300

    asd

--- StudyMate-Apis/src/main/java/com/studymate/apis/controller/WebServicesController.java
+++ StudyMate-Apis/src/main/java/com/studymate/apis/controller/WebServicesController.java
@@ -30,30 +30,23 @@ public class WebServicesController {
                 userDto.getCurriculum(), userDto.getGender());
     }
 
-//    @PostMapping(URLMappingConstants.LOGIN)
-//    @ResponseBody
-//
-//    public ResponseEntity<String> CreateGroup(@RequestBody GroupDto groupDto) {
-//        log.info("Creating new group");
-//        return groupService.createGroup(groupDto.getGroupName(),groupDto.getUniversity(),
-//                groupDto.getCurriculum(),groupDto.getGroupAdmin(),groupDto.getMembers());
+    @PostMapping(URLMappingConstants.LOGIN)
+    @ResponseBody
+    public ResponseEntity<String> Login(@RequestBody UserDto userDto) {
+        log.info("Logging in user");
+        return userService.loginUser(userDto.getUsername(), userDto.getPassword());
+
+    }
 
 
-//    public ResponseEntity<String> Login(@RequestBody UserDto userDto) {
-//        log.info("Logging in user");
-//        return userService.loginUser(userDto.getUsername(), userDto.getPassword());
-//
-//    }
-
-
-//    @PostMapping(URLMappingConstants.CREATE_GROUP)
-//    @ResponseBody
-//    public ResponseEntity<String> CreateGroup(@RequestBody UserDto userDto) {
-//        log.info("Creating new group");
-//      //  return service.createUser(userName,password);
-//        return userService.addUser(userDto.getUsername(), userDto.getPassword(), userDto.getEmail(), userDto.getUniversity(), userDto.getDegree(),
-//                userDto.getCurriculum(), userDto.getGender());
-//    }
+    @PostMapping(URLMappingConstants.CREATE_GROUP)
+    @ResponseBody
+    public ResponseEntity<String> CreateGroup(@RequestBody UserDto userDto) {
+        log.info("Creating new group");
+      //  return service.createUser(userName,password);
+        return userService.addUser(userDto.getUsername(), userDto.getPassword(), userDto.getEmail(), userDto.getUniversity(), userDto.getDegree(),
+                userDto.getCurriculum(), userDto.getGender());
+    }
 
 
 
